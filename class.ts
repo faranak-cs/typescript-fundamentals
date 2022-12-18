76884a1f755e73f0a54345e9a24b62cd9b8cca97
Faran Ahmad Khan <fakhan.cs@gmail.com>
2022-12-18 15:58:15 +0500

    Class definition

---
+++ class.ts
@@ -0,0 +1,30 @@
+// class Point {
+//     x: number;
+//     y: number;
+
+
+// }
+
+
+
+// const pt = new Point();
+
+// pt.x = 2;
+// pt.y = 4;
+
+
+// console.log(`${pt.x}, ${pt.y}`);
+
+
+class School{
+    name : string;
+    strength : number;
+
+
+    constructor(){
+        this.name = "DPS"
+        this.strength = 1000;
+    }
+}
+const school = new School();
+console.log(school);
